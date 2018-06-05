3db4f0fdd380e4d27b92e180d0ebced0b1e16057
leetao <leetao94cn@gmail.com>
2018-06-05 21:25:49 +0800

    modify startup.py

--- startup.py
+++ startup.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from movie import get_all_down_url_list
 from tkinter import *
+from tkinter.ttk import *
 
 class Application(Frame):
     __slots__ = ['movie_entry', 'search_btn', 'entry_style', 'btn_style']
@@ -14,7 +15,7 @@ class Application(Frame):
         self.movie_entry = Entry(self)
         self.movie_entry.pack(side="top")
 
-        self.quit = Button(self, text="搜索", command=self.say_hi, fg="white", bg='#2196f3')
+        self.quit = Button(self, text="搜索", command=self.say_hi)
         self.quit.pack(side="bottom")
 
     def say_hi(self):
